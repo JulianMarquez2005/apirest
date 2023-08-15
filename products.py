dab881fb55213f8491acec7d54f7872a75ea332f
JulianMarquez2005 <119985131+JulianMarquez2005@users.noreply.github.com>
2023-08-15 16:31:07 -0500

    borrado2

--- products.py
+++
@@ -1,6 +0,0 @@
-#vamos a simular los datos que usaremos en nuestra restapi#
-products = [
-    {"name": "laptop", "price": 800, "quantity": 4},
-    {"name": "mouse", "price": 40, "quantity": 10},
-    {"name": "monitor", "price": 400, "quantity": 3},
-]
